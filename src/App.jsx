2332bb4c1ca86a59dc042e807ab78e7833135078
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-08-07 16:06:44 +0300

    fox1

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import IntroSection from "./components/IntroSection";
 import TabsSection from "./components/TabsSection";
 import FeedbackSection from "./components/FeedbackSection";
 import { useState } from "react";
+// import ModernCssBtn from "./components/ModernCss/ModernCssBtn";
 
 export function App() {
   const [tab, settab] = useState("feedback");
   return (
     <>
       <Header />
+
       <main>
         <IntroSection />
-
+        {/* <ModernCssBtn /> */}
         <TabsSection active={tab} onChange={(current) => settab(current)} />
 
         {tab === "main" && (
---
+++ src/components/ModernCss/ModernCssBtn.jsx
@@ -0,0 +1,5 @@
+import "./ModernCssBtn.css";
+
+export default function ModernCssBtn() {
+  return <div className={"box"}>Hello I'm Modern Border!</div>;
+}
